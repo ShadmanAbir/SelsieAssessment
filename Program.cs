acc2749ee226733edb6f343cc09df95567213ca9
Shadman Sakib <shadmanabir@outlook.com>
2024-09-09 18:52:40 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TimeConflict;
+var Cal = new CalculateTime();
+TimeSlot givenSlot = new TimeSlot
+{
+    StartTime = new DateTime(2024, 8, 19, 14, 0, 0),
+    EndTime = new DateTime(2024, 8, 19, 15, 0, 0)
+};
+TimeSlot[] existingSlots = new TimeSlot[] {
+ new TimeSlot
+ {
+ StartTime = new DateTime(2024, 8, 19, 13, 30, 0),
+ EndTime = new DateTime(2024, 8, 19, 14, 30, 0)
+ },
+ new TimeSlot
+ {
+ StartTime = new DateTime(2024, 8, 19, 14, 45, 0),
+ EndTime = new DateTime(2024, 8, 19, 15, 15, 0)
+ },
+ new TimeSlot
+ {
+ StartTime = new DateTime(2024, 8, 19, 16, 0, 0),
+ EndTime = new DateTime(2024, 8, 19, 17, 0, 0)
+ }
+
+};
+
+
+var result = Cal.Calculate(givenSlot, existingSlots);
+foreach (var item in result)
+{
+    Console.WriteLine(item.ToString());
+}
---
+++ TimeSlot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConflict
+{
+    public class TimeSlot
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+
+    public class CalculateTime
+    {
+        public List<bool> Calculate(TimeSlot givenSlot, TimeSlot[] existingSlots)
+        {
+            var result = new List<bool>();
+            if (existingSlots.Length < 1)
+                result.Add(false);
+
+            foreach (var slot in existingSlots)
+            {
+                if (givenSlot.StartTime <= slot.StartTime && givenSlot.EndTime <= slot.EndTime)
+                {
+                    result.Add(true);
+                }
+                else if (givenSlot.StartTime >= slot.StartTime && givenSlot.EndTime <= slot.EndTime)
+                {
+                    result.Add(true);
+                }
+                else if (givenSlot.StartTime <= slot.StartTime && givenSlot.EndTime >= slot.EndTime)
+                {
+                    result.Add(true);
+                }
+                
+                else
+                {
+                    result.Add(false);
+                }
+            }
+            return result;
+        }
+    }
+}
